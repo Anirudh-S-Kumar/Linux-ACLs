1afa09ec27024ed0e75b612a9a83d61dcc011136
Anirudh-S-Kumar <anirudh.skumar.03@gmail.com>
2023-09-11 17:09:27 +0000

    made edits to preproc.sh

--- preproc.sh
+++ preproc.sh
@@ -2,10 +2,10 @@
 
 chmod u+s bin/*
 
-for file in bin/*; do
-    sudo setcap cap_setuid+ep $file
-done
+# for file in bin/*; do
+#    sudo setcap cap_setuid+ep $file
+# done
 
 # change owner for simple_sudo
-sudo chown root bin/simple_sudo
-sudo chgrp root bin/simple_sudo
+# sudo chown root bin/simple_sudo
+# sudo chgrp root bin/simple_sudo
